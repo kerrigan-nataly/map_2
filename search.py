1c19f3691bfb319a97c6da46d8dd784e4abff0aa
Kabachok <94787801+kerrigan-nataly@users.noreply.github.com>
2022-03-27 18:31:42 +0300

    Update search.py
    Задача 3 из урока WEB6 (Поиск аптеки 2.0)

--- search.py
+++ search.py
@@ -1,14 +1,21 @@
 import sys
+import math
 from io import BytesIO
 import requests
 from PIL import Image
 from yandex_map_helper import set_map_params
 
+def distance(coord1, coord2):
+    cat1 = abs(float(coord1[0]) - float(coord2[0]))
+    cat2 = abs(float(coord1[1]) - float(coord2[1]))
+    dist = math.sqrt(cat1 ** 2 + cat2 ** 2)
+    dist *= 111.1  # Перевод в километры
+    return int(dist * 1000)
+
 
 # python search.py Москва, ул. Ак. Королева, 12
-
+# Адрес
 toponym_to_find = " ".join(sys.argv[1:])
-
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
 geocoder_params = {
@@ -17,35 +24,52 @@ geocoder_params = {
     "format": "json"}
 
 response = requests.get(geocoder_api_server, params=geocoder_params)
-
-# Преобразуем ответ в json-объект
 json_response = response.json()
-with open('geo.txt', 'w') as f:
-    f.write(str(json_response))
 
 # Получаем первый топоним из ответа геокодера.
-
 toponym = json_response["response"]["GeoObjectCollection"][
     "featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
-toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-# Дельта
-corners = toponym['boundedBy']['Envelope']
-delta = str(float(corners['upperCorner'].split()[0]) - float(corners['lowerCorner'].split()[0]))
-print(delta)
 
-# Собираем параметры для запроса к StaticMapsAPI:
+toponym_coodrinates = toponym["Point"]["pos"].split(" ")
+toponym_longitude, toponym_lattitude = toponym_coodrinates
 
-map_params = set_map_params(toponym_longitude, toponym_lattitude, delta)
-map_params['pt'] = f"{toponym_longitude},{toponym_lattitude},pm2rdl"
-print(map_params['pt'])
+# Получаем аптеку
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+address_ll = ','.join(toponym_coodrinates)
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    'results': "1",
+    "ll": address_ll,
+    "type": "biz"}
+
+apteka_response = requests.get(search_api_server, params=search_params)
+apteka_json = apteka_response.json()
+
+apteka = apteka_json["features"][0]
+apteka_coodrinates = apteka["geometry"]["coordinates"]
+apteka_longitude, apteka_lattitude = apteka_coodrinates
+#print(apteka_json)
+
+# Создание и вывод карты
+map_params = {'l': 'map'}
+map_params['pt'] = f"{toponym_longitude},{toponym_lattitude},pm2ntl~{apteka_longitude},{apteka_lattitude},pm2rdl"
+
 map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
 
 Image.open(BytesIO(
     response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
+# ВЫвод информации об аптеке
+# Рассчет расстояния
+apteka_data = apteka["properties"]["CompanyMetaData"]
+snippet = [apteka_data["address"],
+           apteka_data["name"],
+           apteka_data["Hours"]["text"],
+           str(distance(apteka_coodrinates, toponym_coodrinates))]
+
+print('\n'.join(snippet))
+
