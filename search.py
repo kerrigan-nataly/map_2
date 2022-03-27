4d17c37f7023c06570ff963fea6c0ba65e0354a0
Kabachok <94787801+kerrigan-nataly@users.noreply.github.com>
2022-03-27 19:09:00 +0300

    Update search.py
    Решение 4 задачи из WEB6(10 аптек)

--- search.py
+++ search.py
@@ -33,7 +33,7 @@ toponym = json_response["response"]["GeoObjectCollection"][
 toponym_coodrinates = toponym["Point"]["pos"].split(" ")
 toponym_longitude, toponym_lattitude = toponym_coodrinates
 
-# Получаем аптеку
+# Получаем аптеки
 search_api_server = "https://search-maps.yandex.ru/v1/"
 api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
@@ -42,34 +42,33 @@ search_params = {
     "apikey": api_key,
     "text": "аптека",
     "lang": "ru_RU",
-    'results': "1",
+    'results': "10",
     "ll": address_ll,
     "type": "biz"}
 
 apteka_response = requests.get(search_api_server, params=search_params)
 apteka_json = apteka_response.json()
 
-apteka = apteka_json["features"][0]
-apteka_coodrinates = apteka["geometry"]["coordinates"]
-apteka_longitude, apteka_lattitude = apteka_coodrinates
-#print(apteka_json)
-
 # Создание и вывод карты
 map_params = {'l': 'map'}
-map_params['pt'] = f"{toponym_longitude},{toponym_lattitude},pm2ntl~{apteka_longitude},{apteka_lattitude},pm2rdl"
+map_params['pt'] = ''
+for apt in apteka_json["features"]:
+    if "Intervals" in apt["properties"]["CompanyMetaData"]["Hours"]["Availabilities"][0]:
+        color = "pm2rdl"
+    elif "TwentyFourHours" in apt["properties"]["CompanyMetaData"]["Hours"]["Availabilities"][0]:
+        color = "pm2gnl"
+    else:
+        color = "pm2grl"
+        
+    lat, lon = apt['geometry']['coordinates']
+    map_params['pt'] += f"{lat},{lon},{color}~"
+
+map_params['pt'] = map_params['pt'][:-1]
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(map_api_server, params=map_params)
 
 Image.open(BytesIO(
     response.content)).show()
-# ВЫвод информации об аптеке
-# Рассчет расстояния
-apteka_data = apteka["properties"]["CompanyMetaData"]
-snippet = [apteka_data["address"],
-           apteka_data["name"],
-           apteka_data["Hours"]["text"],
-           str(distance(apteka_coodrinates, toponym_coodrinates))]
 
-print('\n'.join(snippet))
 
